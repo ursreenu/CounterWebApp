fffab12d4a7d8feee588c30a019249c0e0cd470d
ursreenu <ursreenu@gmail.com>
2016-12-26 23:28:39 +0530

    test git merge
    test

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 //This sample controller
+//Testing the git merge
 @Controller
 public class BaseController {
 
